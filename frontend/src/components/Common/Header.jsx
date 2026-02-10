8b7cef2ffe0cf8505c9e3b6d18ce560526c37baf
Bin-Bajah <2203cmp060@alhikmah.edu.ng>
2026-02-10 17:06:09 +0100

    space commit

---
+++ frontend/src/components/Common/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div>
+        {/* Topbar */}
+        {/* navbar */}
+        {/* Cart Drawer */}
+
+    </div>
+  )
+  
+}
---
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -0,0 +1,14 @@
+import Header from "../Common/Header";
+
+const UserLayout = () => {
+  return (
+    <>
+      <Header />
+      <main className="p-6">
+        <h2 className="text-xl">User layout works ✅</h2>
+      </main>
+    </>
+  );
+};
+
+export default UserLayout;
